67033ca6ee3a7bbb404d2fe79016f66a4c8628e9
MarkusIsaksson1982 <mjisaksson@gmail.com>
2024-11-12 05:49:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,25 @@
 
 var express = require('express');
 var cors = require('cors');
-var multer = require('multer'); // Import multer for handling file uploads
+var multer = require('multer'); 
 require('dotenv').config();
 
 var app = express();
 
-// Configure multer for handling file uploads
 var upload = multer({ dest: 'uploads/' }); // 'uploads/' is the folder where files are temporarily stored
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-// Serve HTML file
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// File upload endpoint
 app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
   if (!req.file) {
     return res.status(400).json({ error: 'No file uploaded' });
   }
 
-  // Send back file metadata as a JSON response
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
@@ -32,7 +28,6 @@ app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
   });
 });
 
-// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port);
